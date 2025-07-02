7ad22bb821018b7fa800879044688394847b43fb
bkumar-27 <b.kumaraswamy.dev@gmail.com>
2025-07-03 00:42:58 +0530

    #

---
+++ 01.Basics/script.js
@@ -0,0 +1,40 @@
+// console.log("Welcome to Javascript")
+
+// log , info ,  warn, error,table
+
+// -- single line comments
+
+/* ------- Multiline comments ---*/
+
+// log
+
+console.log('Iam a Log message');
+
+//info
+
+console.info('Iam an information Message');
+
+// warn
+console.warn('Iam a Warning Message');
+
+//error
+
+console.error('Iam an Error Message ');
+
+//table
+
+let student = {
+	name: 'Kumar',
+	age: 32,
+	course: 'CSC'
+};
+
+console.table(student);
+
+/*-------
+chrome  -> V8 Engine its inbuild in our system
+firefox -> Mozilla ->  spider Monkey (inside of engine to the browser)
+IE - microsoft -> chakra -> 
+
+
+*/
