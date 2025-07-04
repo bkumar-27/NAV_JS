6c12e6a63774f13c9233e72608262d03deee665a
bkumar-27 <b.kumaraswamy.dev@gmail.com>
2025-07-05 01:03:51 +0530

    #

---
+++ 02.Varaiable Declarations/script.js
@@ -0,0 +1,115 @@
+
+// // /*
+
+// // let -> keyword to declare a variable
+
+// // a -> variable name / Referance  varable
+
+// // = -> assignment operator
+// // 10 -> value / data
+
+// // ; -> end of the statement
+// // */
+
+// // /* 1:
+// // let a = 10;
+// // let b = 20;
+// // let sum = a + b;
+// // console.log(sum);
+// // */
+
+// // /* 2:
+// // let a = 10;
+// // let b = 20;
+// // let sum = (a + b);
+// // console.log('The sum of a , b is : ' + sum);
+
+// // */
+
+// // /*
+// // let a = 10;
+// // let b = 20;
+// // let sum = (a + b);
+// // console.log('The sum of ' + a + ',' + b + 'is : ' + sum); //ES5
+
+// // //ES6 -> Template String / template Literal(' ') back ticks
+
+// // console.log(`The sum of  ${a} , ${b} is : ${sum}`); // Es6
+
+
+// // // Rules in Declaring variables
+
+// // // Rules for Variable Declarations
+// // //1. all Variables are Case Sensitive
+// // /*
+// // let name ='Naveen';
+// // console.log(Name) // Error
+
+// // ex:  let name = 'Naveen';
+// //  console.log(name);  // Naveen
+
+// //  let name = 10;
+// //  console.log(name); //10
+
+
+// // let name = "Kumar";
+// // console.log(Name); //Uncaught ReferenceError: Name is not defined
+
+
+// // let name1 = "John";
+// // console.log(name1)
+
+// // let Name = "Ram";
+// // console.log(Name);
+
+// // */
+
+
+
+// // //2 .  All variable name must be in CamelCase
+// // /*
+// // let my_Empolye_Name = "Naveen";
+
+// // let myEmpolyeName = "Naveen";
+
+// // //3) max allowed chars 15
+
+// // let myManagerNameWhileIWasWorkingInfosys = 'John';
+
+// // //4 ) chars  allowed are a-z , A-Z, $, _ , 0-9
+// // /*
+// // let number = 10; // valid
+// // let $number = 20; //valid
+// // let _number = 30; // valid
+// // let number_1 = 40; // valid
+
+// // // 4.1  variable must not starts with a number
+// // let 1number = 50; // Not Valid
+
+// // // variable must not starts with a number
+
+// // let #name = 'John'; //Not Valid
+
+// // *
+
+// // //5 . A variable must Not  re-declared
+
+// // /*
+// let name = 'Naveen';
+// let name = ' Naveen Kumar';
+
+// console.log(name); // Uncaught SyntaxError: Identifier 'name' has already been declared
+
+
+
+
+
+
+
+
+
+
+
+
+
+
