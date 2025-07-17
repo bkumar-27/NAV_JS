e0e232e81293858db0e31ba51c4965eef662fe03
bkumar-27 <b.kumaraswamy.dev@gmail.com>
2025-07-18 00:02:02 +0530

    #functions

---
+++ 09.Funcations/script.js
@@ -0,0 +1,79 @@
+/*
+
+Javascript Functions:
+--------------------------------
+Its a Block of Statements which taken in a set Parameters  and does some process and returns the result
+
+-> re-usability..
+
+
+*/
+
+//print  from 0 -10 diff 1
+/*
+let result = " ";
+for (let i = 0; i <= 10; i++) {
+	result += `${i} `;
+}
+console.log(result);
+*/
+/*
+function primeNumbers() {
+	let result = " ";
+	for (let i = 0; i <= 10; i++) {
+		result += `${i} `;
+	}
+	console.log(result);
+}
+primeNumbers();// Function call/ execution
+
+*/
+
+// Parameters Noting but inputs
+
+/*
+function primeNumbers(endNumber) {
+	let result = " ";
+	for (let i = 0; i <= endNumber; i++) {
+		result += `${i} `;
+	}
+	console.log(result);
+}
+primeNumbers(endNumber: 50); //function cal /Execution
+primeNumbers(endNumber: 100); //function cal /Execution */
+
+
+/*
+function primeNumbers(startNumber, endNumber) {
+	let result = " ";
+	for (let i = startNumber; i <= endNumber; i++) {
+		result += `${i} `
+	}
+	console.log(result);
+
+
+}
+
+primeNumbers(10, 50) //functional call / execution
+
+primeNumbers(100, 500) //functional call / execution */
+
+// increment by the value in 3 values pass the 
+
+
+/* function primeNumbers(startNumber, endNumber, increment) {
+	let result = " ";
+	for (let i = startNumber; i <= endNumber; i += increment) {
+		result += `${i} `
+	}
+	console.log(result);
+
+
+}
+
+primeNumbers(startNumber: 10, endNumber: 50, increment: 5) //functional call / execution
+primeNumbers(startNumber: 100, endNumber: 500 increment: 10) //functional call / execution */
+
+
+
+
---
+++ Test/script.js
@@ -0,0 +1,22 @@
+// // console.log(document.body);
+
+// // var number1 = 5;
+// // number1 = 3;
+// // console.log(number1);
+
+// // console.log(isNaN(NaN));
+
+// // console.time('response in');
+// // alert('Click to continue');
+// // console.timeEnd('response in');
+// // alert('One more time');
+// // console.timeEnd('response in');
+
+
+
+// // console.log('%cHello kumar !', 'color: blue; font-size: xx-large');
+
+// console.timeStamp('msg');
+
+
+
