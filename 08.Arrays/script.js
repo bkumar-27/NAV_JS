dc11f6cef4ed8688f2bd0417a5bdae21966d8e5e
bkumar-27 <b.kumaraswamy.dev@gmail.com>
2025-07-25 02:03:49 +0530

    #Arrays

--- 08.Arrays/script.js
+++ 08.Arrays/script.js
@@ -1,4 +1,4 @@
-// Cration of an Array
+/* /* // Cration of an Array
 // Array : an indexed collection  of elements (object/string/array/boolean/number)
 // Every Array is an Object
 
@@ -211,3 +211,184 @@ console.log(activeEmployees);
 
 
 
+ */
+
+
+
+// Functions of  Arrays
+
+// Shift() -> remove the first elemnet  from Array
+console.log('---------------------- Shift()-----------------------')
+let technologies = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies);
+
+technologies.shift();
+console.log(technologies);
+
+
+
+// unshift() ->  add the first element to array
+console.log('---------------------- unshift()-----------------------')
+let technologies1 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies);
+
+technologies.unshift('Node js');
+console.log(technologies1);
+
+
+
+//Push() ->  add the last element to array
+console.log('---------------------- Push()-----------------------')
+let technologies2 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies);
+
+technologies.push('Node js');
+console.log(technologies2);
+
+
+
+//Pop() -> remove the last element from Array
+console.log('---------------------- unshift()-----------------------')
+let technologies3 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies);
+
+technologies.pop();
+console.log(technologies1);
+
+
+
+
+// sort() -> to sort the Array in Ascending order
+console.log('---------------------- sort()-----------------------')
+let technologies4 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies);
+
+technologies.sort();
+console.log(technologies1);
+
+
+
+// reverse() -> to remove  the Array elements
+console.log('---------------------- reverse()-----------------------')
+let technologies5 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies5);
+
+
+technologies5.reverse();
+console.log(technologies5);
+
+
+
+//reverse order of elements
+
+console.log('---------------------- reverse order -----------------------')
+let technologies6 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies6);
+
+technologies6.sort().reverse();
+console.log(technologies6);
+
+
+
+//splice() -> to remove/ replace from the middle
+//splice(index)-> it Removes all the elements  from the given index
+
+console.log('---------------------- splice(index)-----------------------')
+let technologies7 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies7);
+
+//technologies.splice(index);
+technologies.splice(2); // js to bootstrap all are removed
+technologies.splice(0);// All elements are removed
+console.log(technologies7);
+
+
+
+// Way of Two => in Splice()method
+//splice(index,no-of elements) -> it Removes the given no of  elements from the given Index
+
+console.log('---------------------- splice(index)-----------------------')
+let technologies8 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies7);
+
+technologies8.splice(2, 1); // Here 'Javascript'only Removed
+
+technologies8.splice(2, 2); // here 'javascript' and 'Jquery' both are Removed
+console.log(technologies);
+
+
+
+//splice(index,1, replaace Element) -> it replaces  the given element  at the given index
+
+console.log('---------------------- splice(index)-----------------------')
+let technologies9 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies9);
+technologies9.splice(2, 1, 'Nodejs');
+technologies9.splice(4, 1, 'ReactJs'); // Replaced into Bootstrap;
+console.log(technologies9);
+
+
+//join()-> to Join  the Array elements to string
+
+console.log('---------------------- join() -----------------------');
+
+technologies10 = ['html', 'Css', 'Javascript', 'Jquery', 'bootstrap'];
+console.log(technologies10);
+let techString = technologies.join(" _ ");
+let techString1 = technologies.join(" * ");//separater
+console.log(techString);
+
+console.log('---------------------- split() -----------------------');
+//split() -> to split a string  to an Array
+
+console.log(techString);
+
+//let newTechs = techString.split(separator: '_');
+console.log(newTechs);
+
+
+
+
+//Filter the junior Employees : age <= 30;
+console.log("----------------------filter()---------------");
+juniorEmployee = [];
+for (let employee of employees) {
+	if (employee.age <= 30) {
+		juniorEmployees.push(employee);
+	}
+}
+
+console.log(juniorEmployees);
+
+// based on above code are same but little bit of logic
+
+let jrEmployees = employees.filter(function () {
+
+	return employee.age <= 30;
+
+});
+
+console.log(jrEmployees)
+
+
+
+
+console.log("----------------------filter()---------------");
+// filter  active employees : isActive : true
+
+let activeEmployees = [];
+
+for (let employee of employees) {
+	if (employee.isActive) {
+		activeEmployees.push(employee);
+	}
+}
+
+console.log(activeEmployees)
+
+
+
+let activeEmployees = employee.filter(function (employee) {
+	return employee.isActive;
+});
+console.log(activeEmployees) */
--- 09.Funcations/script.js
+++ 09.Funcations/script.js
@@ -58,7 +58,7 @@ primeNumbers(10, 50) //functional call / execution
 
 primeNumbers(100, 500) //functional call / execution */
 
-// increment by the value in 3 values pass the 
+// increment by the value in 3 values pass the
 
 
 /* function primeNumbers(startNumber, endNumber, increment) {
@@ -73,6 +73,239 @@ primeNumbers(100, 500) //functional call / execution */
 
 primeNumbers(startNumber: 10, endNumber: 50, increment: 5) //functional call / execution
 primeNumbers(startNumber: 100, endNumber: 500 increment: 10) //functional call / execution */
+
+
+
+
+// Functions without  Parameters
+function greet() {
+	console.log('Good Morning');  // Its Normal Function
+}
+greet();
+
+
+// Function Expression
+/* let name = function () {   // Here we can aply : string , number, array any thing we can apply
+
+}
+
+
+let greetMe = function () {
+	console.log('Good Morning')
+};
+
+console.log(greetMe);  // here total function is came as it print
+
+greetMe(); */
+
+
+
+
+// function  with Parameters
+/*
+let wishMe = function (name, age) {
+	let message = `Hello ${name} You are ${age} yrs old`;
+	console.log(message);
+};
+// wishMe(name'john', age 40);
+wishMe("john", 40);
+wishMe("John")//o/p Hello John You are undefined yrs old
+
+// Function  with return Type
+
+let sum = function (a, b) {
+	let result = 2 * (a + b);
+	return result;
+}
+let output = sum(10, 20);
+console.log(output); //60
+
+
+
+
+// Function  with  an object  as Parameter
+
+let printObject = function (obj) {
+	console.log(obj);
+};
+printObject(obj = {
+	name: 'Rajan', age: 20
+});
+
+let mobile = {
+	brand: 'Apple',
+	color: 'Silver',
+	price: 35000
+};
+
+printObject(mobile);
+
+
+//function  with an Array  as a parameter
+
+let printArray = function (array) {
+	let result = '';
+	for (let index in array) {
+		result += `INDEX : ${index} => VALUE : ${array[index]}  `;
+	}
+	console.log(result);
+
+};
+
+printArray(array = [10, 20, 30, 40]);
+
+
+
+
+let colors = ['white', 'black', 'orange', 'purple'];
+printArray(colors);
+
+
+
+// function inside  an Object
+
+let student = {
+	firstName: 'Arujun',
+	lastName: 'Reddy',
+	fullName: function () {
+		return 'Arjun Reddy ';
+
+	}
+};
+
+console.log(student.firstName); // Arujun
+
+
+// let date = (student.fullName);
+// console.log(data);
+
+
+// let data = fullName;
+// console.log(student.data)
+
+console.log(student.fullName);  // Total Function Return
+
+console.log(student.fullName());  //Arjun Reddy  is come
+
+// using This key word
+
+
+let student1 = {
+	firstName: 'Arujann',
+	lastName: 'Redddy',
+	fullName: function () {
+		return `${this.firstName} ${this.lastName}`
+
+	}
+};
+
+console.log(student1.fullName())//Arujann Redddy
+
+
+// Nested  Function
+
+let outerFn = function () {
+	console.log('Iam an outer  Function');
+
+	let innerFn = function () {
+		console.log('Iam an inner Function')
+	}
+
+	innerFn(); // Iam an inner Function
+
+};
+outerFn(); //  Iam an outer
+
+
+
+// method -2
+
+let outerFn1 = function () {
+
+	innerFn2(); // Uncaught ReferenceError: Cannot access 'innerFn2' before initialization
+	console.log('Iam an outer  Function');
+
+	let innerFn2 = function () {
+		console.log('Iam an inner Function')
+	}
+
+
+
+};
+outerFn1(); //  Iam an outer  Function
+
+
+
+
+
+// Twisted  Function
+
+
+let twistedFn = function () {
+	let name = 'John';
+	let printStudent = function () {
+		let student = {
+			name: 'Rajan',
+			age: 20,
+			course: 'CSE',
+
+		};
+		return student;
+
+	}
+	return printStudent;
+
+};
+let innerFn = twistedFn();
+let startObj = innerFn();
+console.log(studentObj.name); */
+
+
+
+//Prepare chicken  Curry with  Functions
+
+
+let glassBowel = function (rawChicken, masala) {
+	// marinating  the Chicken
+
+	let marinatedChiken = `MIXING : (${rawChicken} + ${masala})`;
+
+	return marinatedChiken;
+
+
+
+
+};
+
+let cookingBowl = function (marinatedChiken, water) {
+
+	//cooking logic
+	let cookedChicken = `COOKING : (${marinatedChiken}+${water})`;
+
+	return cookedChicken;
+};
+
+let eatingPlate = function (cookedChicken, rice) {
+	// Eating Logic
+	let eating = `EATING : (${cookedChicken}) + ${rice} `;
+
+	console.log(eating);
+
+
+}
+
+
+let rawChicken = `3kg chicken`;
+let masala = `Garlic,pepper,salt`;
+let marinatedChiken = glassBowel(rawChicken, masala);
+let water = `1Ltr Water`;
+
+let cookedChicken = cookingBowl(marinatedChiken, water);
+let rice = '1Kg Rice'
+
+eatingPlate(cookedChicken, rice);
+
+
 
 
 
