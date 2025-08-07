360ed46b816e677a3efb62e0452584ffecb84ca0
bkumar-27 <b.kumaraswamy.dev@gmail.com>
2025-08-07 16:41:32 +0530

    #

---
+++ 19.Range Selection/script.js
@@ -0,0 +1,20 @@
+//Range Select
+
+
+// format  Number
+
+let formatNumber = (number) => {
+
+	let result = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(number);
+	return result;
+};
+let rangeEl = document.querySelector('#range');
+let amountEl = document.querySelector('#amount')
+
+rangeEl.addEventListener('input', function () {
+
+	let selectedAmount = rangeEl.value;
+	amountEl.innerText = formatNumber(selectedAmount);
+
+
+});
---
+++ 20.SMS Application/script.js
@@ -0,0 +1,9 @@
+//SMS App
+
+let textAreaEL = document.querySelector('#text-area');
+let charCountEL = document.querySelector('#char-count');
+textAreaEL.addEventListener('keyup', function () {
+
+	let textLength = textAreaEL.value.length;
+	charCountEL.innerText = textLength;
+});
